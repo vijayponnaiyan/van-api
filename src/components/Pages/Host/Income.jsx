73e763f1247696c9aa9b62e8642a042bf12049d3
vijayponnaiyan <isreallit@gmail.com>
2024-10-05 16:59:51 +0530

    update

--- src/components/Pages/Host/Income.jsx
+++ src/components/Pages/Host/Income.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function Income() {
   return (
     <div>
-        <div className='p-10' >
-            <h1 className='text-black p-3 font-bold text-4xl'  >Income</h1>
-            <div  className='p-2'>
-            <p>
-               Last day<span className="underline">30 days</span>{' '}
-            </p>
-            </div>
-            <div className="p-2">
+      <div className='p-10'>
+        <h1 className='text-black p-3 font-bold text-4xl'>Income</h1>
+        <div className='p-2'>
+          <p>
+            Last day<span className="underline">30 days</span>{' '}
+          </p>
+        </div>
+        <div className="p-2">
           <p className="text-5xl font-bold">$2,260</p>
         </div>
-
-
-        </div>
-        
-
-
+     
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Pages/Host/Reviews.jsx
+++ src/components/Pages/Host/Reviews.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Reviews() {
   return (
     <div>
-        <div>
-             <h1>Reviews</h1>
+        <div className='p-10  flex '  >
+        <h1 className='text-black p-3 font-bold text-4xl'>Your reviews   </h1>
+        <p> Last day<span className="underline">30 days</span>{' '}</p>
         </div>
     </div>
   )
